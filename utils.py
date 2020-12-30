d1142aa1942dbf7977b158fc08a35764c4998b08
aladin-29 <moin.uddin@par-tec.it>
2020-12-30 12:47:46 +0100

    added ask and mask password functions

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import os
-import time
+import sys
 import shutil
+import time
+import tty
+import termios
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -92,6 +95,77 @@ def cleanup(number_of_days, path):
             remove(item)
     log.info("CleanUP Process has been terminated!")
 
+    
+def get_passwd(prompt="Password: ", mask="*"):
+
+    STR_TYPE = str  # type: type
+    if not isinstance(prompt, STR_TYPE):
+        raise TypeError(
+            "prompt argument must be a str, not %s" % (type(prompt).__name__)
+        )
+    if not isinstance(mask, STR_TYPE):
+        raise TypeError(
+            "mask argument must be a zero- or one-character str, not %s"
+            % (type(prompt).__name__)
+        )
+    if len(mask) > 1:
+        raise ValueError("mask argument must be a zero- or one-character str")
+
+    if mask == "" or sys.stdin is not sys.__stdin__:
+        # Fall back on getpass if a mask is not needed.
+        import getpass as gp
+
+        return gp.getpass(prompt)
+
+    enteredPassword = []  # type: List[str]
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    while True:
+        key = ord(getch())
+        if key == 13:  # Enter key pressed.
+            sys.stdout.write("\n")
+            return "".join(enteredPassword)
+        elif key in (8, 127):  # Backspace/Del key erases previous output.
+            if len(enteredPassword) > 0:
+                # Erases previous character.
+                sys.stdout.write(
+                    "\b \b"
+                )  # \b doesn't erase the character, it just moves the cursor back.
+                sys.stdout.flush()
+                enteredPassword = enteredPassword[:-1]
+        elif 0 <= key <= 31:
+            # Do nothing for unprintable characters.
+            # TODO: Handle Esc, F1-F12, arrow keys, home, end, insert, del, pgup, pgdn
+            pass
+        else:
+            # Key is part of the password; display the mask character.
+            char = chr(key)
+            sys.stdout.write(mask)
+            sys.stdout.flush()
+            enteredPassword.append(char)
+
+
+def getch():
+
+    # type: () -> str
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+        if ch == "\x03":
+            raise KeyboardInterrupt
+    except KeyboardInterrupt:
+        # print("Hey hey hey")
+        sys.exit("\nCTRL ^C Recieved, Houston, we are closing ...!")
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+    
+    
+    
+    
 class config:
     LDAP_SERVER = "ldap://localhost:390"
     BASE_DN = "dc=your-domain,dc=it"  # base dn to search in
